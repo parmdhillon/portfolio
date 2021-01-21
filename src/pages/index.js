635920d363d0b7e9dea55873b3c0019fb8813b5d
Parminder <iparm1996@gmail.com>
2021-01-21 16:20:08 -0500

    Initial Setup

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+import HomeScreen from '../screens/HomeScreen';
+
+export default function Home() {
+  return (
+    <>
+    <Head>
+    <title>Parminder Singh - Front End Developer</title>
+    </Head>
+    
+      <HomeScreen />
+    </>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: ['./src/pages/**/*.js', './src/components/**/*.js'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans:
+          'Mulish, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
